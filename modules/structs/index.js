fa42262d3dfc73c986edc78bf10782b6607437fe
Lorenzo Sabatelli <lorenzo.sabatelli.92@gmail.com>
2020-09-26 12:50:41 +0200

    add comment and argument fallback value to the get method of struct module

--- modules/structs/index.js
+++ modules/structs/index.js
@@ -1,4 +1,10 @@
-function get(name, data){
+
+/**
+ * 
+ * @param {string} name nome struttura
+ * @param {object} data dati da sovrascrivere a quelli di default della struttura richiesta
+ */
+function get(name, data = {}){
     return Object.assign({}, require('./data/' + name) || {}, data);
 }
 
